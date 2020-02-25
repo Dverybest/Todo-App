9cefae13856abea0a38a4745caab493f30498a76
Charles Best <charlesbest8@gmail.com>
2020-02-26 00:17:55 +0100

    Updates

---
+++ precache-manifest.457a7a609e153f74800e03d703e2de17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb2883aa092deb847e46117cdc4ae80a",
+    "url": "/Todo-App/index.html"
+  },
+  {
+    "revision": "dd19f87a008a8d5a1cbd",
+    "url": "/Todo-App/static/css/main.6a11a23d.chunk.css"
+  },
+  {
+    "revision": "ed91433ccc9cfb05fc7f",
+    "url": "/Todo-App/static/js/2.9ac8582a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Todo-App/static/js/2.9ac8582a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd19f87a008a8d5a1cbd",
+    "url": "/Todo-App/static/js/main.b9121a5f.chunk.js"
+  },
+  {
+    "revision": "a082400fd7e7e8f53412",
+    "url": "/Todo-App/static/js/runtime-main.1111e716.js"
+  }
+]);
