d0cb11ba8e5669f3b4a77e7ec103483a74b8d099
Charles Best <charlesbest8@gmail.com>
2020-02-25 22:59:22 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+import Todos from './components/Todos'
+import AddTodo from './components/AddTodo'
 
 function App() {
+  const [todos,setTodos] = useState([
+    {id:1,content:"Add more todo's",isChecked:false},
+  ])
+
+  const deleteTodo = (id) =>{
+    let data = todos.filter(todo=>{
+      return todo.id !== id
+    })
+    setTodos(data)
+  }
+  const addTodo = (todo)=>{
+    todo.id = Math.random();
+    todo.isChecked = false;
+    setTodos([...todos,todo]);
+  }
+  const handleCheckChange =(id)=>{
+    let index = todos.findIndex(todo=>todo.id===id);
+
+    let data = [...todos];
+    data[index].isChecked =!data[index].isChecked
+    setTodos(data)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo-app container">
+      <h1 className="center blue-text">Todo's</h1>
+      <Todos todos={todos} deleteTodo={deleteTodo} handleCheckChange={handleCheckChange}/>
+      <AddTodo addTodo={addTodo}/>
     </div>
   );
 }
 
 export default App;
+ 
---
+++ src/components/AddTodo.js
@@ -0,0 +1,26 @@
+import React,{useState} from 'react';
+
+const AddTodo = ({addTodo})=>{
+
+    const [todo,setTodo] = useState({content:''})
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        if(todo.content!==''){
+            addTodo(todo);
+            setTodo({content:''});
+        }
+    }
+    const handleChange =(e)=>{
+        setTodo({content:e.target.value})
+    }
+    return(
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label>Add new todo and click enter</label>
+                <input type="text" onChange={handleChange} value={todo.content}/>
+            </form>
+        </div>
+    )
+}
+export default AddTodo;
