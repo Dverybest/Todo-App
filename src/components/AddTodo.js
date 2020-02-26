a0836dbafc381eadad9232ffeda4f05140a6bf87
Charles Best <charlesbest8@gmail.com>
2020-02-26 18:02:58 +0100

    updates

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -3,7 +3,7 @@ import React,{useState} from 'react';
 const AddTodo = ({addTodo})=>{
 
     const [todo,setTodo] = useState({content:''})
-
+//yarn add gh-pages --save-dev.... to add git deploy dependency
     const handleSubmit = (e)=>{
         e.preventDefault();
         if(todo.content!==''){
